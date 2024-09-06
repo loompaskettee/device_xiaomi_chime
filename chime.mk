97915a0f265c7395d107768642c0cd77b68a9e9a
HeroBuxx <me@herobuxx.me>
2024-09-06 16:59:09 +0000

    chime: Patch libwvhidl to use libcrypto-v33
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- chime.mk
+++ chime.mk
@@ -182,6 +182,9 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
     android.hardware.drm-service.clearkey
 
+PRODUCT_PACKAGES += \
+    libcrypto-v33
+
 # Fastboot - Dynamic Partition
 PRODUCT_PACKAGES += \
     fastbootd
