a98870ed7c08c4fcf107a5456cae051548ccfca1
chandu078 <chandudyavanapelli03@gmail.com>
2024-09-14 13:55:44 +0200

    chime: Switch to 64 bit only builds
    Change-Id: I0e730663a0e1ad8b18cfa1163b94edf0acd64fff
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- chime.mk
+++ chime.mk
@@ -5,7 +5,7 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Setup dalvik vm configs
@@ -315,8 +315,6 @@ PRODUCT_COPY_FILES += \
 
 # OMX
 PRODUCT_PACKAGES += \
-    android.hardware.media.omx@1.0-impl \
-    android.hardware.media.omx@1.0-service \
     libOmxCore \
     libOmxVdec \
     libOmxVenc \
